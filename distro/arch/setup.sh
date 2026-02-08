73ced94bc12f51ac3facf1c5c6d266d125edd277
Declan Teevan <dt@hexolan.com>
2026-02-08 13:16:30 +0000

    feat(hypr): extend cfgs
    feat(distro): extend stub arch setup script
    
    Extended hypr configs (only base/example config files without
    any themes to this repo for now - pending some polishing/refactoring).
    
    Added stubs for adding soem of my `plymouth` and `sddm` theming to my
    dots repo.

--- distro/arch/setup.sh
+++ distro/arch/setup.sh
@@ -11,12 +11,39 @@
 ARCH_APPS = (
   base-devel
   git
+
+  kitty
+  fastfetch # ofc, ofc
+  zoxide
+
+  neovim
+  lazygit
+  lazydocker
+
+  kleopatra
 )
 
 AUR_APPS = (
-  brave  # disable safe browsing (sends URLS for checking against single API)
+  brave  # disable safe browsing; privacy (sends URLS, hashes - but known DB, for checking against single API)
   firefox
 
+  # bruno
+  # insomnia
+)
+
+# TODO: customise install level within script
+# select custom 'packages' / 'apps' (minimal dots, core apps/cfgs only, etc)
+HYPR_APPS = (
+  hyprland
+  hyprlock
+  hypridle
+  rofi
+  swaync
+  waybar
+)
+
+HYPR_AUR_APPS = (
+  awww-git
 )
 
 # if [[ $HOSTTYPE == "server" ]]; then STANDARD_PKGS+=(tmux) else AUR_PKGS+=(spotify) fi
