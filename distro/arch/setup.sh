ec838ecae1e92a28bb6e1a88f8dc4c25485d9948
Declan Teevan <dt@hexolan.com>
2026-03-24 09:44:39 +0000

    feat(distro): extend arch pkg stubs
    Will add install wrappers to script at some point - when not between
    other things.

--- distro/arch/setup.sh
+++ distro/arch/setup.sh
@@ -35,15 +35,41 @@ AUR_APPS = (
   # insomnia
 )
 
+ARCH_DEV_APPS = (
+  rustup
+  go
+
+  llvm
+  gcc
+)
+
 # Security Apps
+# TODO: config for selinux setups
 ARCH_SECURITY_APPS = (
   apparmor
   firejail
+
+  fwupd
 )
 
 ARCH_SECURITY_AUR_APPS = (
   # NOTE: These apparmor profiles aren't *ALWAYS* the best on Arch without modification
   # apparmor.d
+)
+
+ARCH_NETWORKING = (
+  networkmanager
+  iwd
+
+  # ca-certificates
+  ca-certificates-mozilla
+
+  openssl
+  libressl
+
+  ufw
+  firewalld
+  opensnitch
 )
 
 # TODO: customise install level within script
